04d744f2d02f9b9094f51b23a966843b10777f16
Abderrahmane Benbachir <anis.benbachir@gmail.com>
2017-05-05 01:09:07 -0400

    fix xml analysis

--- bootup-trace/rscript/density.R
+++ bootup-trace/rscript/density.R
@@ -2,7 +2,7 @@
 library(ggplot2)
 data=read.table("./bootup-trace/rscript/overhead_kernel_user_space_functions.csv", header=T, sep=',')
 
-data <- subset(data, overhead >= 0 & overhead < 100)
+data <- subset(data, overhead >= 0 & overhead < 10)
 # 
 pl<-ggplot(data, aes(x = reorder(function_name, overhead, FUN=median),  function_name, y = overhead )) +
   geom_boxplot(outlier.colour = "red", outlier.size=1) +
@@ -11,10 +11,8 @@ pl<-ggplot(data, aes(x = reorder(function_name, overhead, FUN=median),  function
   xlab("Function call") +
   ylab("Overhead %") +
   theme_bw() +
-  theme_bw(base_size = 5) +
+  theme_bw(base_size = 13) +
   theme(legend.position = "top") +
-  theme(axis.text.x = element_text(angle = 90, 
-                                   hjust = 1, 
-                                   vjust = 0))
+  theme(axis.text.x = element_text(angle = 50, hjust = 1))
   # theme(axis.ticks = element_line(size = 1))
 plot(pl)
