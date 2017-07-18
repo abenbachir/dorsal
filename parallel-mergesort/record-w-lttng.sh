90490a2660b6c074772c27ba7a735b1ff158e8cb
Abder Benbachir <anis.benbachir@gmail.com>
2017-07-18 12:53:31 -0400

    upate

--- parallel-mergesort/record-w-lttng.sh
+++ parallel-mergesort/record-w-lttng.sh
@@ -2,6 +2,10 @@ lttng create
 lttng enable-event -u "lttng_ust_cyg*"
 lttng add-context -u -t vpid -t vtid -t procname
 lttng start
-LD_PRELOAD=liblttng-ust-cyg-profile.so ./mergesort
+LD_PRELOAD=liblttng-ust-cyg-profile.so ./mergesort >> tracing-data.csv
+LD_PRELOAD=liblttng-ust-cyg-profile.so ./mergesort >> tracing-data.csv
+LD_PRELOAD=liblttng-ust-cyg-profile.so ./mergesort >> tracing-data.csv
+LD_PRELOAD=liblttng-ust-cyg-profile.so ./mergesort >> tracing-data.csv
+LD_PRELOAD=liblttng-ust-cyg-profile.so ./mergesort >> tracing-data.csv
 lttng stop
 lttng destroy
