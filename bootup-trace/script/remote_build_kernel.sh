bf210a18ce1dd274432aff38e4107e4d8765a475
Abder Benbachir <anis.benbachir@gmail.com>
2017-10-19 19:06:53 -0400

    add new patch

--- bootup-trace/script/remote_build_kernel.sh
+++ bootup-trace/script/remote_build_kernel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-linuxsource="linux"
-target="vm1"
+linuxsource="linux-dirty"
+target="vm"
 # virsh setvcpus --live ubuntu 4
 
 
@@ -10,17 +10,19 @@ target="vm1"
 #scp ~/$linuxsource/kernel/trace/trace_functions_graph.c $target:~/linux/kernel/trace/
 #scp ~/$linuxsource/kernel/hypercall.c $target:~/linux/kernel/
 
-scp ~/$linuxsource/kernel/trace/Makefile $target:~/linux/kernel/trace/
+#scp ~/$linuxsource/kernel/trace/Makefile $target:~/linux/kernel/trace/
 # scp ~/$linuxsource/kernel/trace/trace.c $target:~/linux/kernel/trace/
 # scp ~/$linuxsource/kernel/trace/trace.h $target:~/linux/kernel/trace/
 
-scp ~/$linuxsource/kernel/trace/trace_hypergraph.c $target:~/linux/kernel/trace/
+#scp ~/$linuxsource/kernel/trace/trace_hypergraph.c $target:~/linux/kernel/trace/
 # scp ~/$linuxsource/kernel/trace/trace_hyperbootlevel.c $target:~/linux/kernel/trace/
 # scp ~/$linuxsource/kernel/trace/trace_hypertrace.c $target:~/linux/kernel/trace/
 
 # scp ~/$linuxsource/init/main.c $target:~/linux/init/
 # scp ~/$linuxsource/init/Makefile $target:~/linux/init/
 
+
+scp ~/$linuxsource/arch/x86/kernel/ftrace_64.S $target:~/linux/arch/x86/kernel/
 
 ssh $target 'cd linux; make -j$(nproc) && sudo make modules_install && sudo make install;'
 
